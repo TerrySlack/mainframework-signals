2bbda78cdb53479304354217ee810db4b996aac9
Terry Slack <tslack@mainframework.com>
2024-08-23 16:41:42 -0700

    adding types

--- src/shared/hooks/useSignal.ts
+++ src/shared/hooks/useSignal.ts
@@ -4,7 +4,7 @@ import { useLocation } from "react-router-dom";
 import { createSignal, destroySignal } from "../utils/signals";
 
 export const useSignal = <T>(
-  initialValue: T | Promise<T> | (() => Promise<T>),
+  initialValue: (Partial<T> | T) | Promise<T> | (() => Promise<T>),
   //using the type 'any' here, because TS suddenly started complaining when this hook is used that it can't infer the type being returend.
   //Should be T instead, but it's not working.
 ): [any, (newValue: T | Promise<T> | (() => Promise<T>)) => void] => {
@@ -27,5 +27,5 @@ export const useSignal = <T>(
     };
   }, [location]);
 
-  return [value as T, signal.set as (newValue: T | Promise<T> | (() => Promise<T>)) => void];
+  return [value as T, signal.set as (newValue: (Partial<T> | T) | Promise<T> | (() => Promise<T>)) => void];
 };
